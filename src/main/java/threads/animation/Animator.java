56d7c588e5e1e07b4c154c63aae58b2a40366073
Jonathan Jensen <haiyun211@gmail.com>
2013-11-25 08:47:39 -0700

    Fix animator to work with worker thread and timer

--- src/main/java/threads/animation/Animator.java
+++ src/main/java/threads/animation/Animator.java
@@ -1,21 +1,16 @@
 package threads.animation;
-import java.awt.BorderLayout;
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-
-public class Animator extends JFrame{
+public class Animator extends JFrame implements ActionListener {
 	JButton start, stop;
 	JPanel ballPanel;
 	Ball ball;
 	boolean animate;
+    Timer timer = new Timer(100, this);
 	
 	public Animator(){
 		super("Animation");
@@ -34,24 +29,49 @@ public class Animator extends JFrame{
 		};
 		ball = new Ball(ballPanel);
 		getContentPane().add(ballPanel);
-		
+
+
+
 		start.addActionListener(new ActionListener(){
-			
+
+
 			@Override
 			public void actionPerformed(ActionEvent e){
-				animate = true;
-				while(animate){
-					ball.move();
-					repaint();
-					try{
-						Thread.sleep(100);
-					} catch (InterruptedException ex){
-						ex.printStackTrace();
-					}
-				}
+                System.out.println(e.getActionCommand() + " fired");
+
+                timer.start();
+                worker.execute();
 			}
 		});
+
+        stop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //animate = false;
+                timer.stop();
+
+            }
+        });
+
 	}
+
+    SwingWorker worker = new SwingWorker<Ball, Void>() {
+        @Override
+        protected Ball doInBackground() throws Exception {
+            animate = true;
+            while(animate){
+
+                repaint();
+               /* try{
+                     Thread.sleep(100);
+                } catch (InterruptedException ex){
+                     ex.printStackTrace();
+                }*/
+            }
+
+            return ball;
+        }
+    };
 	
 	public static void main(String[] args){
 		JFrame f = new Animator();
@@ -59,5 +79,10 @@ public class Animator extends JFrame{
 		f.setBounds(300, 300, 300, 200);
 		f.setVisible(true);
 	}
-	
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Timer Event Fired");
+        ball.move();
+    }
 }
